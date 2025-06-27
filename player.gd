70b62ff61f612477c99fbdafee996a74b6da58ce
Shuflduf <shuflduf@gmail.com>
2025-06-26 19:27:39 -0600

    you can now throw empty

--- player.gd
+++ player.gd
@@ -1,20 +1,33 @@
 extends RigidBody3D
 
 @onready var ui: Control = $UI
-@onready var pepsi_bar: TextureProgressBar = $UI/TextureProgressBar
 
 @export var mouse_sens: float = 0.01
 
+var is_pepsi_ready = false
+var ammo = 100
+
+
 func _ready() -> void:
     Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    %PepsiAnim.play(&"reload")
 
 func _process(delta: float) -> void:
-    $UI/AnimatedSprite2D.play(&"default")
-    $UI/AnimatedSprite2D.animation_finished.connect(func():
-        $UI/TextureProgressBar.show()
-        $UI/AnimatedSprite2D.hide()
-    )
-    pepsi_bar.value -= delta * 10
+    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_pepsi_ready:
+        ammo -= delta * 50
+        check_ammo()
+    refresh_pepsi_bar()
+
+func check_ammo():
+    if ammo < 0:
+        is_pepsi_ready = false
+        ammo = 100
+        %PepsiBar.hide()
+        %PepsiAnim.show()
+        %PepsiAnim.play(&"throw")
+
+func refresh_pepsi_bar():
+    %PepsiBar.value = ammo * 0.6
 
 func is_on_floor() -> bool:
     if test_move(transform, Vector3.DOWN*0.1 * get_physics_process_delta_time() ):
@@ -35,3 +48,15 @@ func _input(event: InputEvent) -> void:
 
         $CamPivot.rotate_x(-event.relative.y * mouse_sens)
         $CamPivot.rotation.x = clamp($CamPivot.rotation.x, deg_to_rad(-89), deg_to_rad(89))
+
+
+func _on_pepsi_anim_animation_finished() -> void:
+    match %PepsiAnim.animation:
+        &"reload":
+            is_pepsi_ready = true
+
+            %PepsiBar.show()
+            %PepsiAnim.hide()
+        &"throw":
+
+            %PepsiAnim.play(&"reload")
