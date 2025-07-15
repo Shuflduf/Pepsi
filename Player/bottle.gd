72402952457de0d21c9ba78bacedd6ac4f6d94c6
Shuflduf <shuflduf@shuflduf.xyz>
2025-07-14 18:03:57 -0600

    MORE

--- Player/bottle.gd
+++ Player/bottle.gd
@@ -10,173 +10,173 @@ signal bottle_spawned(bottle: RigidBody3D)
 var pepsi_pos = Vector2.ZERO
 
 enum PepsiState {
-    Melee,
-    Ranged,
-    Firing,
+	Melee,
+	Ranged,
+	Firing,
 }
 
 var is_pepsi_ready = false
 var ammo = 100.0:
-    set(value):
-        ammo = value
-        visuals.value = value
+	set(value):
+		ammo = value
+		visuals.value = value
 var fizz = 0.0:
-    set(value):
-        fizz = value
-        visuals.fizz = value
+	set(value):
+		fizz = value
+		visuals.fizz = value
 
 var current_state = PepsiState.Ranged
 
 func _ready() -> void:
-    visuals.play_anim(&"reload_catch")
+	visuals.play_anim(&"reload_catch")
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventMouseMotion:
-        visuals.offset -= event.relative
+	if event is InputEventMouseMotion:
+		visuals.offset -= event.relative
 
 
 func _process(delta: float) -> void:
-    visuals.offset = lerp(visuals.offset, Vector2.ZERO, delta * 20)
-    #if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_pepsi_ready and current_state == PepsiState.Ranged:
+	visuals.offset = lerp(visuals.offset, Vector2.ZERO, delta * 20)
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and is_pepsi_ready and current_state == PepsiState.Ranged:
 
 
-    if current_state == PepsiState.Firing and is_pepsi_ready:
-        ammo -= delta * 50
-        check_ammo()
+	if current_state == PepsiState.Firing and is_pepsi_ready:
+		ammo -= delta * 50
+		check_ammo()
 
-        fizz -= delta * 2
-        fly.emit(fizz)
+		fizz -= delta * 2
+		fly.emit(fizz)
 
-    if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-        swing()
-        drink(delta)
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		swing()
+		drink(delta)
 
-    if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-        aim()
-        throw()
-    else:
-        unaim()
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
+		aim()
+		throw()
+	else:
+		unaim()
 
-    if Input.is_action_pressed(&"debug_throw"):
-        throw(true)
+	if Input.is_action_pressed(&"debug_throw"):
+		throw(true)
 
 
 func check_ammo():
-    if ammo < 0:
-        unaim()
-        is_pepsi_ready = false
-        visuals.play_anim(&"reload_throw")
+	if ammo < 0:
+		unaim()
+		is_pepsi_ready = false
+		visuals.play_anim(&"reload_throw")
 
 func _unhandled_key_input(event: InputEvent) -> void:
-    if event.is_action_pressed(&"switch"):
-        switch_state()
+	if event.is_action_pressed(&"switch"):
+		switch_state()
 
 func switch_state():
-    if !is_pepsi_ready:
-        return
-    is_pepsi_ready = false
-    match current_state:
-        PepsiState.Ranged:
-            visuals.play_anim(&"switch_melee")
-            current_state = PepsiState.Melee
-        PepsiState.Melee:
-            visuals.play_anim(&"switch_ranged")
-            current_state = PepsiState.Ranged
-        PepsiState.Firing:
-            is_pepsi_ready = true
+	if !is_pepsi_ready:
+		return
+	is_pepsi_ready = false
+	match current_state:
+		PepsiState.Ranged:
+			visuals.play_anim(&"switch_melee")
+			current_state = PepsiState.Melee
+		PepsiState.Melee:
+			visuals.play_anim(&"switch_ranged")
+			current_state = PepsiState.Ranged
+		PepsiState.Firing:
+			is_pepsi_ready = true
 
 func drink(delta: float):
-    if current_state != PepsiState.Ranged:
-        return
-    if !is_pepsi_ready:
-        return
+	if current_state != PepsiState.Ranged:
+		return
+	if !is_pepsi_ready:
+		return
 
-    ammo -= delta * 50
-    check_ammo()
+	ammo -= delta * 50
+	check_ammo()
 
 func aim():
-    if current_state != PepsiState.Ranged:
-        return
-    if !is_pepsi_ready:
-        return
+	if current_state != PepsiState.Ranged:
+		return
+	if !is_pepsi_ready:
+		return
 
-    current_state = PepsiState.Firing
-    is_pepsi_ready = false
-    visuals.play_anim(&"aim")
+	current_state = PepsiState.Firing
+	is_pepsi_ready = false
+	visuals.play_anim(&"aim")
 
 func unaim():
-    if current_state != PepsiState.Firing:
-        return
-    if !is_pepsi_ready:
-        return
+	if current_state != PepsiState.Firing:
+		return
+	if !is_pepsi_ready:
+		return
 
-    %Particles.emitting = false
-    current_state = PepsiState.Ranged
-    is_pepsi_ready = false
-    visuals.play_anim(&"unaim")
+	%Particles.emitting = false
+	current_state = PepsiState.Ranged
+	is_pepsi_ready = false
+	visuals.play_anim(&"unaim")
 
 func swing():
-    if current_state != PepsiState.Melee:
-        return
-    if !is_pepsi_ready:
-        return
+	if current_state != PepsiState.Melee:
+		return
+	if !is_pepsi_ready:
+		return
 
-    is_pepsi_ready = false
-    visuals.play_anim(&"swing")
+	is_pepsi_ready = false
+	visuals.play_anim(&"swing")
 
-    swung.emit()
-    fizz += 0.5
+	swung.emit()
+	fizz += 0.5
 
 func throw(debug = false):
-    if !debug:
-        if current_state != PepsiState.Melee:
-            return
-        if !is_pepsi_ready:
-            return
-    print("THROWWW")
+	if !debug:
+		if current_state != PepsiState.Melee:
+			return
+		if !is_pepsi_ready:
+			return
+	print("THROWWW")
 
-    is_pepsi_ready = false
-    ammo = 100
-    fizz = 0
+	is_pepsi_ready = false
+	ammo = 100
+	fizz = 0
 
-    var new_bottle: RigidBody3D = bottle_prop.instantiate()
-    add_child(new_bottle)
-    new_bottle.position = %StartPos.global_position
-    new_bottle.rotation = %StartPos.global_rotation
-    bottle_spawned.emit(new_bottle)
+	var new_bottle: RigidBody3D = bottle_prop.instantiate()
+	add_child(new_bottle)
+	new_bottle.position = %StartPos.global_position
+	new_bottle.rotation = %StartPos.global_rotation
+	bottle_spawned.emit(new_bottle)
 
 
-    visuals.play_anim(&"reload_catch")
+	visuals.play_anim(&"reload_catch")
 
 
 func _on_anim_handler_animation_finished(anim_name: StringName) -> void:
-    match anim_name:
-        &"reload_catch":
-            visuals.play_anim(&"ranged")
-            is_pepsi_ready = true
-            current_state = PepsiState.Ranged
-            #ammo = 100
-            #fizz = 0
-        &"reload_throw":
-            ammo = 100
-            fizz = 0
-            visuals.play_anim(&"reload_catch")
-        &"switch_melee":
-            is_pepsi_ready = true
-            visuals.play_anim(&"melee")
-        &"switch_ranged":
-            is_pepsi_ready = true
-            visuals.play_anim(&"ranged")
-        &"swing":
-            is_pepsi_ready = true
-            visuals.play_anim(&"melee")
-        &"aim":
-            is_pepsi_ready = true
-            visuals.play_anim(&"firing")
-            %Particles.emitting = true
-        &"unaim":
-            is_pepsi_ready = true
-            visuals.play_anim(&"ranged")
+	match anim_name:
+		&"reload_catch":
+			visuals.play_anim(&"ranged")
+			is_pepsi_ready = true
+			current_state = PepsiState.Ranged
+			#ammo = 100
+			#fizz = 0
+		&"reload_throw":
+			ammo = 100
+			fizz = 0
+			visuals.play_anim(&"reload_catch")
+		&"switch_melee":
+			is_pepsi_ready = true
+			visuals.play_anim(&"melee")
+		&"switch_ranged":
+			is_pepsi_ready = true
+			visuals.play_anim(&"ranged")
+		&"swing":
+			is_pepsi_ready = true
+			visuals.play_anim(&"melee")
+		&"aim":
+			is_pepsi_ready = true
+			visuals.play_anim(&"firing")
+			%Particles.emitting = true
+		&"unaim":
+			is_pepsi_ready = true
+			visuals.play_anim(&"ranged")
 
 func _physics_process(delta: float) -> void:
-    fizz = clamp(fizz - (delta / 2), 0, 10)
+	fizz = clamp(fizz - (delta / 2), 0, 10)
--- enemy.gd
+++ enemy.gd
@@ -5,27 +5,31 @@ extends PhysicsEntity
 
 var hit = false
 var hit_cooldown = 0.0
+var player: Player = null
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group(&"Player")
 
 func _physics_process(delta: float) -> void:
-    hit_cooldown += delta
-    if is_on_floor() and hit_cooldown > 0.1:
-        hit = false
-        hit_cooldown = 0.0
+	hit_cooldown += delta
+	if is_on_floor() and hit_cooldown > 0.1:
+		hit = false
+		hit_cooldown = 0.0
 
-    var player: RigidBody3D = get_tree().get_first_node_in_group(&"Player")
-    if player and !hit:
-        agent.target_position = player.global_position
-        var cur_pos = global_position
-        var next_path_pos = agent.get_next_path_position()
-        var dir = cur_pos.direction_to(next_path_pos)
-        if dir.y > -0.9 and is_on_floor():
-            print("JUMP")
-            apply_central_impulse(Vector3.UP * 12)
-        dir = Vector3(dir.x, 0.0, dir.z).normalized()
+	if player and !hit:
+		agent.target_position = player.global_position
+		var cur_pos = global_position
+		var next_path_pos = agent.get_next_path_position()
+		var dir = cur_pos.direction_to(next_path_pos)
+		DebugDraw2D.set_text("dir", dir)
+		if dir.y > 0.7 and is_on_floor():
+			apply_central_impulse(Vector3.UP * 8)
+			
+		dir = Vector3(dir.x, 0.0, dir.z).normalized()
+		
 
+		#dir.y = 0
+		apply_central_force(dir * delta * ground_speed)
 
-        #dir.y = 0
-        apply_central_force(dir * delta * ground_speed)
-
-        var look_dir = atan2(dir.x, dir.z)
-        $Visuals.rotation.y = lerp_angle($Visuals.rotation.y, look_dir, delta * 10)
+		var look_dir = atan2(dir.x, dir.z)
+		$Visuals.rotation.y = lerp_angle($Visuals.rotation.y, look_dir, delta * 10)
