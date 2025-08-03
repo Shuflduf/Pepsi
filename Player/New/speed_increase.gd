9150051fa7c850e15bc789faccd78935a2f66f48
Shuflduf <shuflduf@gmail.com>
2025-08-02 21:38:26 -0600

    slight fov change for increased speed

--- Player/New/speed_increase.gd
+++ Player/New/speed_increase.gd
@@ -4,10 +4,13 @@ extends BottleComponent
 @export var increased_speed = 6000.0
 @export var increased_max = 10.0
 @export var increased_max_air = 8.0
+@export var increased_fov = 95.0
+@export var cam: Camera3D
 
 @onready var base_speed = player.ground_speed
 @onready var max_speed = player.max_speed
 @onready var max_air_speed = player.max_air_speed
+@onready var base_fov = cam.fov
 
 var disabled = false
 var factor: float = 1.0
@@ -19,6 +22,7 @@ func _physics_process(delta: float) -> void:
     if Input.is_action_pressed(&"debug"):
         factor += delta * 8.0
 
+    cam.fov = remap(factor, 1.0, 2.0, base_fov, increased_fov)
     player.ground_speed = remap(factor, 1.0, 2.0, base_speed, increased_speed)
     player.max_speed = remap(factor, 1.0, 2.0, max_speed, increased_max)
     player.max_air_speed = remap(factor, 1.0, 2.0, max_air_speed, increased_max_air)
